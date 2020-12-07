09ca118791e792800dd16ffa1d65d7214274d6d1
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-12-07 19:13:44 -0300

    testing: if input if being read

--- dist/index.js
+++ dist/index.js
@@ -152,7 +152,7 @@ async function run() {
     // const chapterApiURL = core.getInput('chapterApiURL', { required: true }) || process.env.CONTENT_CHAPTER_API_URL;
     // const versionApiURL = core.getInput('versionApiURL', { required: true }) || process.env.CONTENT_VERSION_API_URL;
     const filesPath = core.getInput('dirPath') || process.env.FILES_PATH;
-
+    console.log(filesPath)
     const arrayOfChapters = await buildChapters(filesPath);
     console.log(arrayOfChapters);
     // @Todo: disparar criação de capitulos via service.js
