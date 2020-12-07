278085219692722db79b889fd7d6dcb38edc835d
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-12-07 18:53:33 -0300

    test: testing inputs

--- dist/index.js
+++ dist/index.js
@@ -148,10 +148,10 @@ async function run() {
   try {
     core.info('Tryhard Action Rolling');
 
-    const apiKey = core.getInput('apiKey') || process.env.CONTENT_API_KEY;
-    const chapterApiURL = core.getInput('chapterApiURL') || process.env.CONTENT_CHAPTER_API_URL;
-    const versionApiURL = core.getInput('versionApiURL') || process.env.CONTENT_VERSION_API_URL;
-    const filesPath = core.getInput('dirPath') || process.env.FILES_PATH;
+    const apiKey = core.getInput('apiKey', { required: true }) || process.env.CONTENT_API_KEY;
+    const chapterApiURL = core.getInput('chapterApiURL', { required: true }) || process.env.CONTENT_CHAPTER_API_URL;
+    const versionApiURL = core.getInput('versionApiURL', { required: true }) || process.env.CONTENT_VERSION_API_URL;
+    const filesPath = core.getInput('dirPath', { required: true }) || process.env.FILES_PATH;
 
     const arrayOfChapters = await buildChapters(filesPath);
     console.log(arrayOfChapters);
