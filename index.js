a2f20a50e436ee79e5e47d10924ee7c3bd30120c
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2020-12-07 19:02:36 -0300

    testing envs access

--- index.js
+++ index.js
@@ -11,17 +11,19 @@ async function run() {
   try {
     core.info('Tryhard Action Rolling');
 
-    const apiKey = core.getInput('apiKey') || process.env.CONTENT_API_KEY;
-    const chapterApiURL = core.getInput('chapterApiURL') || process.env.CONTENT_CHAPTER_API_URL;
-    const versionApiURL = core.getInput('versionApiURL') || process.env.CONTENT_VERSION_API_URL;
+    // const apiKey = core.getInput('apiKey', { required: true }) || process.env.CONTENT_API_KEY;
+    // const chapterApiURL = core.getInput('chapterApiURL', { required: true }) || process.env.CONTENT_CHAPTER_API_URL;
+    // const versionApiURL = core.getInput('versionApiURL', { required: true }) || process.env.CONTENT_VERSION_API_URL;
     const filesPath = core.getInput('dirPath') || process.env.FILES_PATH;
 
     const arrayOfChapters = await buildChapters(filesPath);
-
+    console.log(arrayOfChapters);
     // @Todo: disparar criação de capitulos via service.js
-    const chapterIds = await createChapters(arrayOfChapters, chapterApiURL, apiKey);
+    // const chapterIds = await createChapters(arrayOfChapters, chapterApiURL, apiKey);
 
     // @Todo: disparar criação de versão via service.js
+
+    core.setOutput('result', Buffer.from(arrayOfChapters.join(',')).toString('base64'));
   } catch (error) {
     core.setFailed(error.message);
   }
