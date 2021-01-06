661fd1955c7522faad5d17e388eb74d262d00211
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2021-01-06 19:23:05 -0300

    feat: implementa testes para novas alteracoes de blob e s3

--- files.test.js
+++ files.test.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const files = require('./files');
 const gitCommands = require('./git');
+const s3 = require('./s3');
 
 jest.mock('./git');
+jest.mock('./s3');
 
 const readMockFile = (path) => fs.readFileSync(path, 'utf8');
 
@@ -61,5 +63,45 @@ describe('Files', () => {
     expect(chapterObj).toHaveProperty('yamlCommitId', 'd9771871c0cadc48e3d4141f93004b9c25d7201a');
     expect(chapterObj).toHaveProperty('contentMd', rawMarkdownContent);
     expect(chapterObj).toHaveProperty('contentYaml', rawYamlContent);
+  });
+
+  test('Generates md5 hash of asset blob', () => {
+    const assetPath = '__mocks__/fixtures/assets/static/agile/scrum.png';
+    const assetBlob = readMockFile(assetPath);
+
+    gitCommands.getBlobContent
+      .mockResolvedValueOnce(assetBlob);
+
+    const blobMd5 = files.generateHashOfAssetBlob(assetBlob);
+
+    expect(typeof blobMd5).toBe('string');
+    expect(blobMd5).toEqual('c0c1cebaf505a08a0c63d2437ad338c7');
+  });
+
+  test('Builds a new asset path URL', () => {
+    const assetPath = '__mocks__/fixtures/assets/static/agile/scrum.png';
+
+    const newHashUrl = files.buildAssetHashUrl(assetPath, 'c0c1cebaf505a08a0c63d2437ad338c7');
+
+    expect(typeof newHashUrl).toBe('string');
+    expect(newHashUrl).toEqual('__mocks__/fixtures/assets/static/agile/scrum-c0c1cebaf505a08a0c63d2437ad338c7.png');
+  });
+
+  test('Builds array of assets new hash urls', async () => {
+    const assetPath = '__mocks__/fixtures/assets/static/agile/scrum.png';
+    const assetBlob = readMockFile(assetPath);
+
+    gitCommands.getBlobContent
+      .mockResolvedValueOnce(assetBlob);
+
+    s3.uploadToBucket
+      .mockResolvedValueOnce({ ETag: 'bebda3f165aede5d08136413d13dca70' });
+
+    const urlHashObj = await files.processAssetContent(assetPath);
+
+    expect(typeof urlHashObj).toBe('object');
+    expect(urlHashObj).toEqual({
+      [assetPath]: '__mocks__/fixtures/assets/static/agile/scrum-213c790c4129428a74486324d08e78e7.png',
+    });
   });
 });
---
+++ s3.test.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const s3Service = require('./s3');
+
+const readMockFile = (path) => fs.readFileSync(path, 'utf8');
+const putObjectOutputMock = { promise: jest.fn().mockResolvedValue({ ETag: 'bebda3f165aede5d08136413d13dca70' }) };
+const putObjectMock = jest.fn(() => putObjectOutputMock);
+const mockS3 = {
+  putObject: putObjectMock,
+};
+
+jest.mock('aws-sdk', () => ({ S3: jest.fn(() => mockS3) }));
+
+describe('S3 Client Actions', () => {
+  afterEach(() => jest.resetAllMocks());
+
+  test('Uploads a new Object to Bucket', async () => {
+    const assetPath = '__mocks__/fixtures/assets/static/agile/scrum.png';
+    const rawAssetBlob = readMockFile(assetPath);
+
+    const result = await s3Service.uploadToBucket(rawAssetBlob, assetPath);
+
+    expect(typeof result).toBe('string');
+    expect(result).toEqual('bebda3f165aede5d08136413d13dca70');
+    expect(putObjectMock).toHaveBeenCalled();
+  });
+});
