55366b710bfdf86fa7dace7685a25a949c85f030
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2021-01-11 15:20:13 -0300

    feat: Adiciona default sha id pra development

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function run() {
     const chapterApiURL = core.getInput('chapterApiURL') || process.env.CONTENT_CHAPTER_API_URL;
     const versionApiURL = core.getInput('versionApiURL') || process.env.CONTENT_VERSION_API_URL;
     const mergedAt = core.getInput('mergedAt') || Date.parse(new Date());
-    const mergeCommitId = core.getInput('mergeCommitId') || '';
+    const mergeCommitId = core.getInput('mergeCommitId') || '7044d626ad61a8011a0ee8ad78e16c89f3c781f7';
     const filesPath = core.getInput('dirPath') || process.env.FILES_PATH;
 
     const arrayOfChapters = await buildChapters(filesPath);
