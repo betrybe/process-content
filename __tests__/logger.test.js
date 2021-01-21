1f57d8e9dd187c2520ef23d03e2cffb8d2036304
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2021-01-21 12:15:18 -0300

    feat: sugestoes de codereview ao logger.test.js

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -2,6 +2,10 @@ const core = require('@actions/core');
 const logger = require('../src/logger');
 
 describe('Logger OutPuts', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Invokes error output when logging an error', () => {
     const spyError = jest.spyOn(core, 'error');
 
@@ -14,12 +18,15 @@ describe('Logger OutPuts', () => {
 
     logger.error(logBody);
     expect(spyError).toHaveBeenCalled();
-
-    spyError.mockRestore();
+    expect(spyError).toHaveBeenCalledTimes(4);
+    expect(spyError).toHaveBeenCalledWith('path: /content/toot/file');
+    expect(spyError).toHaveBeenCalledWith('step: Chapter');
+    expect(spyError).toHaveBeenCalledWith('statusCode: 400');
+    expect(spyError).toHaveBeenCalledWith('message: Failed at executing');
   });
 
   test('Invokes setFailed output when logging an failed action', () => {
-    const spyError = jest.spyOn(core, 'error');
+    const spyError = jest.spyOn(core, 'setFailed');
 
     const logBody = {
       path: '/content/toot/file',
@@ -30,12 +37,15 @@ describe('Logger OutPuts', () => {
 
     logger.setFailed(logBody);
     expect(spyError).toHaveBeenCalled();
-
-    spyError.mockRestore();
+    expect(spyError).toHaveBeenCalledTimes(4);
+    expect(spyError).toHaveBeenCalledWith('path: /content/toot/file');
+    expect(spyError).toHaveBeenCalledWith('step: Chapter');
+    expect(spyError).toHaveBeenCalledWith('statusCode: 500');
+    expect(spyError).toHaveBeenCalledWith('message: Failed at executing');
   });
 
   test('Invokes info output when logging an info', () => {
-    const spyError = jest.spyOn(core, 'error');
+    const spyError = jest.spyOn(core, 'info');
 
     const logBody = {
       message: 'Success',
@@ -43,7 +53,7 @@ describe('Logger OutPuts', () => {
 
     logger.info(logBody);
     expect(spyError).toHaveBeenCalled();
-
-    spyError.mockRestore();
+    expect(spyError).toHaveBeenCalledTimes(1);
+    expect(spyError).toHaveBeenCalledWith('message: Success');
   });
 });
