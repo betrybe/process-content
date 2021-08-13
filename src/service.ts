0ed6d301b3027afd91789af7e5a376ede360a275
Lucas Viana <vianadossantoslucas@gmail.com>
2021-08-13 20:11:52 -0300

    feat: adiciona tipagem a service.ts

--- src/service.ts
+++ src/service.ts
@@ -1,13 +1,28 @@
-const axios = require('axios');
-const core = require('@actions/core');
-const logger = require('./logger');
+import axios, { AxiosResponse } from 'axios';
+import core from '@actions/core';
+import logger from './logger';
+import type { AssetFileInfo, Chapter } from './files';
 
-const CHUNK_SIZE = core.getInput('chunkSize') || process.env.CHUNK_SIZE;
-const INTERVAL_BETWEEN_CHUNKS = core.getInput('intervalBetweenChunks') || process.env.INTERVAL_BETWEEN_CHUNKS;
-const MAX_SERVICE_RETRY = core.getInput('maxServiceRetry') || process.env.MAX_SERVICE_RETRY || 15;
-const HEALTH_CHECK_INTERVAL = core.getInput('healthCheckInterval') || process.env.HEALTH_CHECK_INTERVAL || 10000;
+const CHUNK_SIZE = parseInt(core.getInput('chunkSize')) || process.env.CHUNK_SIZE;
+const INTERVAL_BETWEEN_CHUNKS = parseInt(core.getInput('intervalBetweenChunks')) || process.env.INTERVAL_BETWEEN_CHUNKS;
+const MAX_SERVICE_RETRY = parseInt(core.getInput('maxServiceRetry') )|| process.env.MAX_SERVICE_RETRY || 15;
+const HEALTH_CHECK_INTERVAL = parseInt(core.getInput('healthCheckInterval')) || process.env.HEALTH_CHECK_INTERVAL || 10000;
 
-const handleChapterError = (chapter) => {
+type ChapterResponse = {
+  data: {
+    chapter_id: number;
+  }
+  message: string;
+}
+
+type VersionBody = {
+  merge_commit_id: string;
+  pull_request_merged_at: number;
+  pull_request_id: string | number;
+  chapter_ids: number[];
+}
+
+const handleChapterError = (chapter: AxiosResponse<ChapterResponse>) => {
   const filePath = JSON.parse(chapter.config.data).path;
   const errorMessage = chapter.data.message ? chapter.data.message : chapter.statusText;
 
@@ -22,12 +37,13 @@ const handleChapterError = (chapter) => {
   return logger.error(logBody);
 };
 
-const handleChaptersResult = (createdChaptersResult) =>
+const handleChaptersResult = (createdChaptersResult: AxiosResponse<ChapterResponse>[]) =>
   createdChaptersResult.reduce((resultAccumulator, currentResult) => {
     const newResultAccumulator = { ...resultAccumulator };
     if (currentResult.status === 200) {
       newResultAccumulator.results = [
-        ...newResultAccumulator.results, currentResult.data.data.chapter_id,
+        ...newResultAccumulator.results,
+        currentResult.data.data.chapter_id,
       ];
       return newResultAccumulator;
     }
@@ -39,11 +55,11 @@ const handleChaptersResult = (createdChaptersResult) =>
     }
 
     return newResultAccumulator;
-  }, { results: [], success: true });
+  }, { results: [] as number[], success: true });
 
-const configHeaders = (apiKey) => ({ headers: { 'x-api-content-key': apiKey } });
+const configHeaders = (apiKey: string) => ({ headers: { 'x-api-content-key': apiKey } });
 
-const createChapter = (apiUrl, body, arrayOfAssets, apiKey) => {
+const createChapter = (apiUrl: string, body: Chapter, arrayOfAssets: AssetFileInfo[], apiKey: string) => {
   const headerObj = configHeaders(apiKey);
   const bodyObj = {
     path: body.path,
@@ -54,10 +70,10 @@ const createChapter = (apiUrl, body, arrayOfAssets, apiKey) => {
     assets_map: arrayOfAssets,
   };
 
-  return axios.post(apiUrl, bodyObj, headerObj);
+  return axios.post<ChapterResponse>(apiUrl, bodyObj, headerObj);
 };
 
-const chunkArray = (myArray, chunkSize) => {
+function chunkArray<T>(myArray: Array<T>, chunkSize: number) {
   const results = [];
 
   while (myArray.length) {
@@ -67,39 +83,40 @@ const chunkArray = (myArray, chunkSize) => {
   return results;
 };
 
-const createChaptersChunk = (arrayOfChapters) => chunkArray(arrayOfChapters, CHUNK_SIZE);
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const createChaptersChunk = (arrayOfChapters: Chapter[]) => chunkArray(arrayOfChapters, CHUNK_SIZE);
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const createChapters = async (arrayOfChapters, arrayOfAssets, apiUrl, apiKey) => {
+export const createChapters = async (arrayOfChapters: Chapter[], arrayOfAssets: AssetFileInfo[], apiUrl: string, apiKey: string) => {
   const groupOfChapters = createChaptersChunk(arrayOfChapters);
 
   core.info(`Número de Grupos: #${groupOfChapters.length}`);
 
-  const result = await groupOfChapters.reduce((chain, chapters, index) => (
-    chain.then(async (previousResults) => {
-      core.info(`Iniciando envio grupo ${(index + 1)}...`);
-      const responses = await Promise.all(chapters.map((chapter) =>
-        createChapter(apiUrl, chapter, arrayOfAssets, apiKey).catch((e) => e.response)));
+  const result = await groupOfChapters.reduce(async (chain, chapters, index) => {
+    const previousResults = await chain;
 
-      await sleep(INTERVAL_BETWEEN_CHUNKS);
-      core.info('Envio finalizado.');
+    core.info(`Iniciando envio grupo ${(index + 1)}...`);
+    const chapterPromises = chapters.map((chapter) => createChapter(apiUrl, chapter, arrayOfAssets, apiKey));
 
-      return [...responses, ...previousResults || []];
-    })
-  ), Promise.resolve());
+    const responses: AxiosResponse<ChapterResponse>[] = await Promise.all(chapterPromises).catch((e) => e.response);
+
+    await sleep(INTERVAL_BETWEEN_CHUNKS);
+    core.info('Envio finalizado.');
+
+    return [...responses, ...previousResults || []]; 
+  }, Promise.resolve() as unknown as Promise<AxiosResponse<ChapterResponse>[]>);
 
   core.info('::Todos os grupos de requests finalizados::');
 
   return handleChaptersResult(result);
 };
 
-const createVersion = (apiUrl, apiKey, versionBodyObj) => {
+export const createVersion = (apiUrl: string, apiKey: string, versionBodyObj: VersionBody) => {
   const headerObj = configHeaders(apiKey);
 
   return axios.post(apiUrl, versionBodyObj, headerObj);
 };
 
-const checkForApplication = (apiUrl, apiKey, retries = MAX_SERVICE_RETRY) => {
+export const checkForApplication = (apiUrl: string, apiKey: string, retries = MAX_SERVICE_RETRY): Promise<{ applicationReady: boolean }> => {
   const headerObj = configHeaders(apiKey);
 
   if (retries === 0) return Promise.resolve({ applicationReady: false });
@@ -116,10 +133,4 @@ const checkForApplication = (apiUrl, apiKey, retries = MAX_SERVICE_RETRY) => {
       core.info(`retrying(${retries})... ${apiUrl}`);
       return checkForApplication(apiUrl, apiKey, retries - 1);
     });
-};
-
-module.exports = {
-  createChapters,
-  createVersion,
-  checkForApplication,
 };
