deba1819b0140faefe17d89d76c3599e685257f7
Lucas Viana <vianadossantoslucas@gmail.com>
2021-08-13 20:11:17 -0300

    feat: adiciona tipagem a files.ts

--- src/files.ts
+++ src/files.ts
@@ -1,31 +1,71 @@
-const core = require('@actions/core');
-const crypto = require('crypto');
-const path = require('path');
-const gitCommands = require('./git');
-const s3 = require('./s3');
-const utils = require('./utils');
+import crypto from 'crypto';
+import path from 'path';
 
-const extractFileInfo = async (filePath) => {
-  const commitId = await gitCommands.getCommitId(filePath);
+import * as core from '@actions/core'
+import * as gitCommands from './git';
+import * as s3 from './s3';
+import * as utils from './utils';
+
+type MarkdownExtension = '*.md';
+type YamlExtension = '*.yaml';
+
+type ChapterFilePaths = {
+  markdownPath: string;
+  yamlPath: string;
+}
+
+export type Chapter = {
+  path: string
+} & MarkdownFileInfo & YamlFileInfo;
+
+export type MarkdownFileInfo = {
+  markdownCommitId: string;
+  contentMd: string | void;
+}
+
+export type YamlFileInfo = {
+  yamlCommitId: string;
+  contentYaml: string | void;
+};
+
+export type AssetFileInfo = {
+  assetContent?: string;
+}
+
+function isMarkdownExtension(extension: string): extension is MarkdownExtension {
+  return extension === '.md';
+}
+
+function isYamlExtension(extension: string): extension is YamlExtension {
+  return extension === '.yaml';
+}
+
+
+export async function extractFileInfo(filePath: MarkdownExtension): Promise<MarkdownFileInfo>;
+export async function extractFileInfo(filePath: YamlExtension): Promise<YamlFileInfo>;
+export async function extractFileInfo(filePath: string): Promise<AssetFileInfo>;
+export async function extractFileInfo(filePath: MarkdownExtension | YamlExtension | string) {
+  const commitId = await gitCommands.getCommitId(filePath) ?? '';
   const blobContent = await gitCommands.getBlobContent(commitId, filePath);
 
-  if (path.extname(filePath) === '.md') {
+  if (isMarkdownExtension(path.extname(filePath))) {
     return {
       markdownCommitId: commitId,
       contentMd: blobContent,
-    };
-  } if (path.extname(filePath) === '.yaml') {
+    } as MarkdownFileInfo;
+  } if (isYamlExtension(path.extname(filePath))) {
     return {
       yamlCommitId: commitId,
       contentYaml: blobContent,
-    };
+    } as YamlFileInfo;
   }
 
-  return { assetContent: blobContent };
+  return { assetContent: blobContent } as AssetFileInfo;
 };
 
-const buildChapterObj = async (chapterObj) => {
-  const { markdownPath, yamlPath } = chapterObj;
+export const buildChapterObj = async (chapterObj: ChapterFilePaths) => {
+  const markdownPath = chapterObj.markdownPath as MarkdownExtension;
+  const yamlPath = chapterObj.yamlPath as YamlExtension;
 
   const markdownObj = await extractFileInfo(markdownPath);
   const markdownYaml = await extractFileInfo(yamlPath);
@@ -34,28 +74,28 @@ const buildChapterObj = async (chapterObj) => {
     path: markdownPath,
     ...markdownObj,
     ...markdownYaml,
-  };
+  } as Chapter;
 };
 
-const groupFiles = (filesArr) => filesArr.reduce((groupedFiles, currentPath, index, array) => {
+export const groupFiles = (filesArr: string[]) => filesArr.reduce((groupedFiles, currentPath, index, array) => {
   if (index === array.length) return groupedFiles;
 
   if (path.extname(currentPath).includes('.yaml')) return groupedFiles;
 
-  const filesMatch = utils.verifyFileMatching(currentPath, array[index + 1]);
+  const filesMatch = utils.verifyFileMatching(currentPath, array[index + 1] ?? '');
 
   if (filesMatch) {
     const markdownPath = currentPath;
-    const yamlPath = array[index + 1];
+    const yamlPath = array[index + 1] ?? '';
 
     return [...groupedFiles, { markdownPath, yamlPath }];
   }
 
   return groupedFiles;
-}, []);
+}, [] as ChapterFilePaths[]);
 
-const buildChapters = async (chapterPath) => {
-  const arrayOfFiles = await gitCommands.getFiles(chapterPath);
+export const buildChapters = async (chapterPath: string) => {
+  const arrayOfFiles = await gitCommands.getFiles(chapterPath) ?? '';
 
   const sanitizedArrayOfFiles = utils.sanitizeFilesArray(arrayOfFiles);
 
@@ -63,11 +103,11 @@ const buildChapters = async (chapterPath) => {
 
   core.info(`Processing ${chapterArrayOfObj.length} Chapters`);
   return Promise.all(
-    chapterArrayOfObj.map((chapterObj) => buildChapterObj(chapterObj)),
+    chapterArrayOfObj.map((chapterObj: ChapterFilePaths) => buildChapterObj(chapterObj)),
   );
 };
 
-const buildAssetHashUrl = (assetPath, blobHash) => {
+export const buildAssetHashUrl = (assetPath: string, blobHash: string) => {
   const extension = path.extname(assetPath);
   const pathSuffix = assetPath.substring(0, assetPath.lastIndexOf('.')).split('static/').pop();
   const newHashUrl = `${pathSuffix}-${blobHash}${extension}`;
@@ -75,48 +115,34 @@ const buildAssetHashUrl = (assetPath, blobHash) => {
   return newHashUrl;
 };
 
-const generateContentMd5Hash = (fileContent) => {
-  const contentMd5 = crypto.createHash('md5').update(fileContent).digest('hex');
-  return contentMd5;
-};
+export const generateContentMd5Hash = (fileContent: string) => crypto.createHash('md5').update(fileContent).digest('hex');
 
-const getAssetsFiles = async (assetsPath) => {
-  const arrayOfAssets = await gitCommands.getFiles(assetsPath);
+export const getAssetsFiles = async (assetsPath: string) => {
+  const arrayOfAssets = await gitCommands.getFiles(assetsPath) ?? '';
 
   const sanitizedArrayOfAssets = utils.sanitizeFilesArray(arrayOfAssets);
 
   return sanitizedArrayOfAssets;
 };
 
-const processAssetContent = async (assetPath) => {
+export const processAssetContent = async (assetPath: string) => {
   const { assetContent } = await extractFileInfo(assetPath);
 
-  const assetContentMd5 = generateContentMd5Hash(assetPath, assetContent);
+  const assetContentMd5 = generateContentMd5Hash(assetContent ?? '');
   const assetUrlHash = buildAssetHashUrl(assetPath, assetContentMd5);
 
   const location = await s3.uploadToBucket(assetUrlHash, assetPath);
   const s3UrlLocation = utils.urlSanitizer(location);
-  const relativeAssetPath = assetPath.split('static').pop();
+  const relativeAssetPath = assetPath.split('static').pop() ?? '';
   core.info(`Asset: ${s3UrlLocation} sucessfully uploaded`);
   return { [relativeAssetPath]: s3UrlLocation };
 };
 
-const buildAssets = async (assetsPath) => {
+export const buildAssets = async (assetsPath: string) => {
   const arrayOfAssets = await getAssetsFiles(assetsPath);
 
   core.info(`Processing ${arrayOfAssets.length} Assets`);
   return Promise.all(
     arrayOfAssets.map((assetPath) => processAssetContent(assetPath)),
   );
-};
-
-module.exports = {
-  buildChapters,
-  groupFiles,
-  buildChapterObj,
-  extractFileInfo,
-  buildAssets,
-  generateContentMd5Hash,
-  buildAssetHashUrl,
-  processAssetContent,
 };
